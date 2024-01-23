944bfb2d6fbbce42585ccee174e08bfd5052588c
nodir2208 <norinboyevnortoy544@gmail.com>
2024-01-23 20:41:09 +0500

    intiger-2

---
+++ main/index.js
@@ -0,0 +1,38 @@
+//const son = parseInt(prompt("Ikki xonali butun son kiriting: "));
+
+// Sonning raqamlarini aniqlash
+//const A = Math.floor(son / 10); // Birinchi xonani topish
+//const B = son % 10; // Ikkinchi xonani topish
+
+// Yig'indini va ko'paytmasini topish
+//const summa = A + B;
+//const kopaytma = A * B;
+
+// Natijani chiqarish
+//console.log(`Raqamlari yig'indisi: ${summa}`);
+//console.log(`Raqamlari ko'paytmasi: ${kopaytma}`);
+
+// 2 - masala :
+//const son = parseInt(prompt("Ikki xonali butun son kiriting: "));
+
+//const n = 12;
+//const result = 2 * Math.pow((n + 3), 2);
+//console.log(result);
+
+// 3 - masala :
+//let s;
+//let a = 8;
+//let b = 5;
+//s = Math.sqrt(Math.sqrt((a**3 - b**3)  + (a**3 + b**3)));
+//console.log(s);
+
+// 4 - masala :
+let s;
+let a = 4.5;
+let b = 9.6;
+s = 9*a**2*b - 27*a**2*b**2 + 15*b**2
+console.log(s.toFixed(2));
+
+
+
+
