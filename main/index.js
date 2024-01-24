c216f4212525e2cf1d86d6d5d511f822be35bcbf
nodir2208 <norinboyevnortoy544@gmail.com>
2024-01-24 23:25:26 +0500

    modul

--- main/index.js
+++ main/index.js
@@ -27,12 +27,52 @@
 //console.log(s);
 
 // 4 - masala :
-let s;
-let a = 4.5;
-let b = 9.6;
-s = 9*a**2*b - 27*a**2*b**2 + 15*b**2
-console.log(s.toFixed(2));
+//let s;
+//let a = 4.5;
+//let b = 9.6;
+//s = 9*a**2*b - 27*a**2*b**2 + 15*b**2
+//console.log(s.toFixed(2));
 
+//5 - masala :
 
+//let s
+//let a = 4;
+//let b = 9;
+//s = (1/6*Math.sqrt(a) + 1/3*Math.sqrt(b))**2;
+//console.log(s);
 
+//6 - masala :
+
+//let s;
+//let r = 3;
+//let n = 2;
+//let a = 4;
+//let b = 5;
+//s = (1 + r/100)**n /Math.sqrt(a**2 + b**2);
+//console.log(s);
+
+//7 - masala :
+
+//let s;
+//let x1 = -1.4;
+//let y1 = 7.9;
+//let x2 = 6.1;
+//let y2 = 9.9;
+//s = Math.sqrt(x2 - x1) **2 + Math.sqrt(y2 - y1) **2;
+//console.log(s);
+
+//8 - masala :
+
+let x1 = -9.7;
+let y1 = -9.9;
+let x2 = 0.0;
+let y2 = 2.9;
+let x3 = -0.1;
+let y3 = 5.2;
+let a = Math.sqrt((x2 - x1)**2 + (y2 - y1)**2);
+let b = Math.sqrt((x3 - x2)**2 + (y3 - y2)**2);
+let c = Math.sqrt((x1 - x3)**2 + (y1 - y3)**2);
+let p;
+p = (a + b + c) / 2;
+console.log(p);
 
